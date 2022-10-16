7e073005b4616f01747bfda7d9f564c2f5754781
VJS <minsung_96@naver.com>
2022-10-16 23:59:09 +0900

    Ipify app fetch

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -1,5 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useSelector } from 'react-redux';
 
-const CountryTracks = () => <div>CountryTracks</div>;
+import { Error, Loader, SongCard } from '../components'
+
+const CountryTracks = () => {
+    const [country, setCountry] = useState('')
+    const [loading, setLoading] = useState(true)
+    const { activeSong, isPlaying} = useSelector((state) => state.player)
+
+    useEffect(() => {
+        axios.get('https://geo.ipify.org/api/v2/country?apiKey=at_LLMrDzYCfEbmDBsp916vBwDf9WqzG')
+        .then((res) => setCountry(res?.data?.location?.country))
+        .catch((error) => console.log(error))
+        .finally(() => setLoading(false))
+    }, [country])
+    
+
+    return (
+        <div>
+
+        </div>
+    )
+}
 
 export default CountryTracks;
